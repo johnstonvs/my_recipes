ae66dd06afd7f5a0e3eb85c2f1ec773bcaabd52a
johnston <johnstonvs@gmail.com>
2023-05-11 15:04:26 -0500

    completes first 2 tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import LandingPage from './LandingPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Landing Page', () => {
+
+  test('contains a header with "My Recipes" text', async () => {
+    render(<LandingPage />);
+    screen.getByRole('heading');
+    await screen.findByText('My Recipes')
+      .then(text => {
+        let heading = screen.getByText('My Recipes');
+        expect(heading).toBeInTheDocument();
+      })
+  })
+
+  test('contains "There are no recipes to list" text', async () => {
+    render(<LandingPage />);
+    const element = screen.getByText('There are no recipes to list');
+    expect(element).toBeInTheDocument();
+  })
+
+})
---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <>
+      <h1>My Recipes</h1>
+      <p>There are no recipes to list</p>
+    </>
+  )
+}
+
+export default LandingPage;
